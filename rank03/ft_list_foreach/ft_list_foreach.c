6640b634700bc1d4edc9d037881f76c9bee6ea81
Ignacio Melero Carracedo <imelero-@c2r17s1.42madrid.com>
2025-11-10 20:32:22 +0100

    ft_list_remove_if

--- rank03/ft_list_foreach/ft_list_foreach.c
+++ rank03/ft_list_foreach/ft_list_foreach.c
@@ -10,3 +10,43 @@ void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
 		begin_list = begin_list->next;
 	}
 }
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_data(void *data)
+{
+    printf("%s\n", (char *)data);
+}
+
+// ---------- Función auxiliar para crear nodos ----------
+t_list *create_node(void *data)
+{
+    t_list *new_node = malloc(sizeof(t_list));
+    if (!new_node)
+        return NULL;
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+int main(void)
+{
+    // Crear lista: "uno" -> "dos" -> "tres" -> NULL
+    t_list *n1 = create_node("uno");
+    t_list *n2 = create_node("dos");
+    t_list *n3 = create_node("tres");
+
+    n1->next = n2;
+    n2->next = n3;
+
+    printf("Elementos de la lista:\n");
+    ft_list_foreach(n1, &print_data);
+
+    // Liberar memoria
+    free(n3);
+    free(n2);
+    free(n1);
+
+    return 0;
+}*/
